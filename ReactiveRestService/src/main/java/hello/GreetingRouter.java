64d465fd301d4100296e17184a50b0f429befd03
SudhakarK <featuriz@gmail.com>
2022-10-17 07:58:08 +0530

    INITIAL update

---
+++ ReactiveRestService/src/main/java/hello/GreetingRouter.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration(proxyBeanMethods = false)
+public class GreetingRouter {
+	@Bean
+	public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+		return RouterFunctions.route(
+				RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+				greetingHandler::hello);
+	}
+}
